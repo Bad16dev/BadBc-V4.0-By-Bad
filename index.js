0420d8e9da38adc0ac72f3ebfa4f09cb42040cf2
Bad16dev <71897530+Bad16dev@users.noreply.github.com>
2021-06-24 16:30:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,19 +10,20 @@ app.use('/ping', (req, res) => {
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-var prefix = "$"; // تعديل مهم جدا
-var statuses = [`Ra3d YT`, `Ra3d's BC`, `Hehehe`];
-var timers = 2;
-const owners = ["id1", "id2", "id3"];
+var prefix = "0"; ///ضع هنا البريفيكس الخاص ببوتك
+///Put Here Prefix Of Ur Bot
+var status = [`Bad One`, `BadBcV4.0`];
+var timers = 5;
+const botowner = ["810262257447469116", "id2", "id3"];
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
   client.user.setStatus("dnd");
   var timeing = Math.floor(timers * 1000);
   setInterval(function() {
-    var lengthesof = statuses.length;
+    var lengthesof = status.length;
     var amounter = Math.floor(Math.random() * lengthesof);
-    client.user.setActivity(statuses[amounter], { type: "" });
+    client.user.setActivity(status[amounter], { type: "" });
   }, timeing);
 });
 
@@ -35,23 +36,22 @@ client.on("message", message => {
         message.author.username,
         message.author.displayAvatarURL({ dynamic: true })
       )
-      .setFooter(`Developed by Ra3d `, client.user.displayAvatarURL())
       .setThumbnail(client.user.displayAvatarURL())
       .setDescription(
-        `> **${client.user.username} 's Help commands\n> Available Commands : " 6 " Command\n> Prefix : \`${prefix}\`**\n> **Language : English :flag_gb:**`
+        `> **${client.user.username} 's Help Cmds\n> Commands Count : " 6 " Command\n> Prefix : \`${prefix}\`**\n`
       )
       .addFields(
         {
-          name: "Public",
+          name: "Broadcast Cmds",
           value: `\`${prefix}bc\` , \`${prefix}obc\`, \`${prefix}ping\``
         },
         {
-          name: "Admins",
-          value: `\`${prefix}setname\` , \`${prefix}setavatar\` `
+          name: "Owners Cmds",
+          value: `\`${prefix}changename\` , \`${prefix}changeavatar\` `
         },
         {
-          name: "Extra",
-          value: `\`invite\` , \`help\``
+          name: "Other",
+          value: `\`addme\``
         }
       );
     message.channel.send(help);
@@ -72,7 +72,7 @@ client.on("message", message => {
         message.author.username,
         message.author.displayAvatarURL({ dynamic: true })
       )
-      .addField(`Error :x:`, `Please type your broadcast message !`)
+      .addField(`\`Write Something To Send\``)
       .setTimestamp()
       .setFooter(
         message.author.username,
@@ -80,15 +80,15 @@ client.on("message", message => {
       );
     if (!args) return message.channel.send(message.author, noargs);
     message.guild.members.cache
-      .filter(m => m.presence.status !== "online")
       .forEach(m => {
         if (m.user.bot) return;
         m.send(`${args}\n ${m}`)
+     
           .then(() => {
-            console.log(`I Could Send To : ${m.user.tag} ✅`);
+            console.log(`I Send To : ${m.user.tag} ✅`);
           })
           .catch(function() {
-            console.log(`I Couldn't Send To : ${m.user.tag} ❌ `);
+            console.log(`I Dont Send To : ${m.user.tag} ❌ `);
           });
       });
     let embed = new Discord.MessageEmbed()
@@ -97,11 +97,7 @@ client.on("message", message => {
         message.author.displayAvatarURL({ dynamic: true })
       )
       .setDescription(
-        `📬 : تـم ارسـال رسـالـتـك الـى : \`${
-          message.guild.members.cache.filter(
-            m => m.presence.status !== "online"
-          ).size
-        }\` `
+        `📬 : Send To : \`${message.guild.memberCount}\` `
       )
       .setTimestamp()
       .setFooter(
@@ -109,7 +105,7 @@ client.on("message", message => {
         message.author.displayAvatarURL({ dynamic: true })
       );
     message.channel
-      .send(`جـاري الارسال الرساله الى جميع الاعضاء ..`)
+      .send(`...`)
       .then(me => {
         me.edit(message.author, embed);
       });
@@ -130,7 +126,7 @@ client.on("message", message => {
         message.author.username,
         message.author.displayAvatarURL({ dynamic: true })
       )
-      .addField(`Error :x:`, `Please type your broadcast message !`)
+      .addField(`\`Write Something To Send\``)
       .setTimestamp()
       .setFooter(
         message.author.username,
@@ -143,10 +139,10 @@ client.on("message", message => {
         if (m.user.bot) return;
         m.send(`${args}\n ${m}`)
           .then(() => {
-            console.log(`I Could Send To : ${m.user.tag} ✅`);
+            console.log(`I Send To : ${m.user.tag} ✅`);
           })
           .catch(function() {
-            console.log(`I Couldn't Send To : ${m.user.tag} ❌ `);
+            console.log(`I Dont Send To : ${m.user.tag} ❌ `);
           });
       });
     let embed = new Discord.MessageEmbed()
@@ -155,7 +151,7 @@ client.on("message", message => {
         message.author.displayAvatarURL({ dynamic: true })
       )
       .setDescription(
-        `📬 : تـم ارسـال رسـالـتـك الـى : \`${
+        `📬 : Send To : \`${
           message.guild.members.cache.filter(
             m => m.presence.status !== "offline"
           ).size
@@ -167,7 +163,7 @@ client.on("message", message => {
         message.author.displayAvatarURL({ dynamic: true })
       );
     message.channel
-      .send(`جـاري الارسال الرساله الى جميع الاعضاء ..`)
+      .send(`...`)
       .then(me => {
         me.edit(message.author, embed);
       });
@@ -176,16 +172,16 @@ client.on("message", message => {
 
 client.on("message", async message => {
   if (message.content.startsWith(prefix + "ping")) {
-    message.channel.send("Pinging..").then(m => {
+    message.channel.send("...").then(m => {
       m.edit(
-        `\`\`\`javascript\nDiscord API : ${Math.round(
+        `\`\`\`javascript\nPing : ${Math.round(
           client.ws.ping
         )} ms\n\`\`\` `
       );
     });
   }
-   if (message.content.startsWith(prefix + "invite")) {
-    message.channel.send("creating an invite link..").then(m => {
+   if (message.content.startsWith(prefix + "addme")) {
+    message.channel.send("...").then(m => {
       let embed = new Discord.MessageEmbed()
       .setAuthor(
         message.author.username,
@@ -208,7 +204,7 @@ client.on("message", message => {
   if (message.content.startsWith(prefix + "setname")) {
     let args = message.content.split(" ");
     let botnameee = args.slice(1).join(" ");
-    if (!owners.includes(message.author.id))
+    if (!botowner.includes(message.author.id))
       return message.channel.send(
         `** :x: Only Owners Can   Use this Command **`
       );
@@ -221,10 +217,10 @@ client.on("message", message => {
       me.edit(` Done !`);
     });
   }
-  if (message.content.startsWith(prefix + "setavatar")) {
+  if (message.content.startsWith(prefix + "changeavatar")) {
     let args = message.content.split(" ");
     let botnameee = args.slice(1).join(" ");
-    if (!owners.includes(message.author.id))
+    if (!botowner.includes(message.author.id))
       return message.channel.send(
         `** :x: Only Owners Can   Use this Command **`
       );
@@ -237,6 +233,7 @@ client.on("message", message => {
       me.edit(` Done !`);
     });
   }
+
 });
 
 client.login(process.env.token);
